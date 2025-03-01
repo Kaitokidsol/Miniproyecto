bd00a639958206f5279d94a9d0f434c25ed9c052
Javier Wu <222D2895@live.uem.es>
2025-03-01 15:37:30 +0100

    01/03/2025 Aiming
    estados de aiming y animator

--- Assets/Scripts/AimStates/AimStateManager.cs
+++ Assets/Scripts/AimStates/AimStateManager.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 
 public class AimStateManager : MonoBehaviour
 {
+    AimBaseState currentState;
+    public HipFireState Hip = new HipFireState();
+    public AimState Aim = new AimState();
+
     [SerializeField] float mouseSense = 1;
-    float xAxis, yAxis;
     [SerializeField] Transform camFollowPos;
+    float xAxis, yAxis;
+
+    [HideInInspector] public Animator anim;
+    [HideInInspector] public CinemachineVirtualCamera vCam;
+    public float adsFov = 40;
+    [HideInInspector] public float hipFov;
+    [HideInInspector] public float currentFov;
+    public float fovSmoothSpeed;
     
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        vCam = GetComponentInChildren<CinemachineVirtualCamera>();
+        hipFov = vCam.m_Lens.FieldOfView;
+        anim = GetComponentInChildren<Animator>();
+        SwitchState(Hip);
     }
 
     // Update is called once per frame
@@ -20,6 +36,8 @@ public class AimStateManager : MonoBehaviour
         xAxis += Input.GetAxisRaw("Mouse X") * mouseSense;
         yAxis -= Input.GetAxisRaw("Mouse Y") * mouseSense;
         yAxis = Mathf.Clamp(yAxis, -80, 80);
+
+        currentState.UpdateState(this);
     }
 
     private void LateUpdate()
@@ -27,5 +45,11 @@ public class AimStateManager : MonoBehaviour
         camFollowPos.localEulerAngles = new Vector3(yAxis, camFollowPos.localEulerAngles.y,
             camFollowPos.localEulerAngles.z);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
+    }
+
+    public void SwitchState(AimBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
     }
 }
