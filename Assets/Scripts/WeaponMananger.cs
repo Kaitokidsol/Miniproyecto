f767cbfef12fc1fb36faa56ecd3e0af18d65e5a5
Javier Wu <222D2895@live.uem.es>
2025-03-05 14:23:22 +0100

    05/03/2025 Weapon aiming
    El aiming con arma y un poco del shooting

---
+++ Assets/Scripts/WeaponMananger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WeaponMananger : MonoBehaviour
+{
+    [SerializeField] float fireRate;
+    float fireRateTimer;
+    [SerializeField] bool semiAuto;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fireRateTimer = fireRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldFire()) Fire();
+    }
+
+    bool ShouldFire()
+    {
+        fireRateTimer += Time.deltaTime;
+        if (fireRateTimer < fireRate) return false;
+        if (semiAuto && Input.GetKeyDown(KeyCode.Mouse0)) return true;
+        if (!semiAuto && Input.GetKey(KeyCode.Mouse0)) return true;
+        return false;
+    }
+
+    void Fire()
+    {
+        fireRateTimer = 0;
+        Debug.Log("Fire");
+    }
+}
